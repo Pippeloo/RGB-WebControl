334afbd59d7e732af3070e0a4b75b17aa5bbe4c0
Pippeloo <julestorfs@amps.be>
2022-04-03 19:41:44 +0200

    First really working version

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,8 @@
-# syntax=docker/dockerfile:1
-FROM arm32v7/python:3
+FROM arm64v8/python:latest
 
-WORKDIR /usr/src/app
+COPY ./UDPsocket/UDP_Server_4CH.py ./
 
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir rpi.gpio
+RUN pip install rpi_ws281x adafruit-circuitpython-neopixel
 
-ADD UDPsocket/UDP_Server_4CH.py /
-
-#RUN apt-get update && apt-get install python3-pip -y
-#RUN export CFLAGS=-fcommon
-RUN apt-get install gcc python-dev
-RUN python3-m pip install RPi.GPIO -y
-
-RUN python3 -m pip install rpi_ws281x adafruit-circuitpython-neopixel
-RUN python3 -m pip install --force-reinstall adafruit-blinka
-
-CMD [ "python3", "UDP_Server_4CH.py" ]
-
-
+CMD ["python", "UDP_Server_4CH.py"]
--- UDPsocket/UDP_Server_4CH.py
+++ UDPsocket/UDP_Server_4CH.py
@@ -1,54 +1,65 @@
-# install the following libraries with pip
-# sudo pip install rpi_ws281x adafruit-circuitpython-neopixel
-# sudo python3 -m pip install --force-reinstall adafruit-blinka
-
 import board
 import neopixel
 import socket
 import json
 
 
-host_name = socket.gethostname()
-host_addr = socket.gethostbyname(host_name + ".local")
-
-UDP_IP = host_addr
+UDP_IP = '0.0.0.0'
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind((UDP_IP, UDP_PORT))
 
+debug = False
+
 while True:
     data, addr = sock.recvfrom(1024)
-    print("received message: ", data.decode("utf-8"))
+    try:
+        msg = json.loads(data.decode('ascii'))
 
-    msg = json.loads(data.decode('ascii'))
+        if ("debug" in msg):
+            if (msg["debug"] == True):
+                debug = True
+                # return a message that debug is on
+                sock.sendto(b"Debug is on", addr)
+            else:
+                debug = False
+                # return a message that debug is off
+                sock.sendto(b"Debug is Disabled", addr)
 
-    if ("p" in msg) and ("ch" in msg):
-        if msg['ch'] == 1:
-            pixels_1 = neopixel.NeoPixel(board.D18, msg['p'])
-            print("Configurated Channel 1!")
-        elif msg['ch'] == 2:
-            pixels_2 = neopixel.NeoPixel(board.D21, msg['p'])
-            print("Configurated Channel 2!")
-        elif msg['ch'] == 3:
-            pixels_3 = neopixel.NeoPixel(board.D12, msg['p'])
-            print("Configurated Channel 3!")
-        elif msg['ch'] == 4:
-            pixels_4 = neopixel.NeoPixel(board.D10, msg['p'])
-            print("Configurated Channel 4!")
+        if (debug == True):
+            sock.sendto(b"RECEIVED: " + data, addr)
 
-    if "ch" in msg:
-        if msg['ch'] == 1:
-            if ("r" in msg) and ("g" in msg) and ("b" in msg):
-                pixels_1.fill((msg['r'], msg['g'], msg['b']))
-        elif msg['ch'] == 2:
-            if ("r" in msg) and ("g" in msg) and ("b" in msg):
-                pixels_2.fill((msg['r'], msg['g'], msg['b']))
-                print(msg)
-        elif msg['ch'] == 3:
-            if ("r" in msg) and ("g" in msg) and ("b" in msg):
-                pixels_3.fill((msg['r'], msg['g'], msg['b']))
-        elif msg['ch'] == 4:
-            if ("r" in msg) and ("g" in msg) and ("b" in msg):
-                pixels_4.fill((msg['r'], msg['g'], msg['b']))
+        if ("p" in msg) and ("ch" in msg):
+            if msg['ch'] == 1:
+                pixels_1 = neopixel.NeoPixel(board.D18, msg['p'])
+                print("Configurated Channel 1!")
+            elif msg['ch'] == 2:
+                pixels_2 = neopixel.NeoPixel(board.D21, msg['p'])
+                print("Configurated Channel 2!")
+            elif msg['ch'] == 3:
+                pixels_3 = neopixel.NeoPixel(board.D12, msg['p'])
+                print("Configurated Channel 3!")
+            elif msg['ch'] == 4:
+                pixels_4 = neopixel.NeoPixel(board.D10, msg['p'])
+                print("Configurated Channel 4!")
+
+        if "ch" in msg:
+            if msg['ch'] == 1:
+                if ("r" in msg) and ("g" in msg) and ("b" in msg):
+                    pixels_1.fill((msg['r'], msg['g'], msg['b']))
+            elif msg['ch'] == 2:
+                if ("r" in msg) and ("g" in msg) and ("b" in msg):
+                    pixels_2.fill((msg['r'], msg['g'], msg['b']))
+                    print(msg)
+            elif msg['ch'] == 3:
+                if ("r" in msg) and ("g" in msg) and ("b" in msg):
+                    pixels_3.fill((msg['r'], msg['g'], msg['b']))
+            elif msg['ch'] == 4:
+                if ("r" in msg) and ("g" in msg) and ("b" in msg):
+                    pixels_4.fill((msg['r'], msg['g'], msg['b']))
+
+    except Exception as e:
+        # send e to the client
+        sock.sendto(b"Error: " + str(e), addr)
